af9e7b10196278d76242471d5d186d1bf2fd2c2b
Mike Bessuille <mikebessuille@hotmail.com>
2024-04-25 15:42:54 -0400

    React component is updating from data sent from electron
    Woot!  It works!

--- src/MikeApp.js
+++ src/MikeApp.js
@@ -2,18 +2,24 @@ import './App.css';
 import CPPResultComponent from './callcppworker.js';
 import { channels } from './constants';
 import React, { useEffect } from 'react';
+import { useState } from 'react';
 
 function MikeApp() {
+  const [myVal, setMyVal] = useState(0);
+
+  // Method to send information from React to Electron (called from the button click)
   const sendTestEventMike = () => {
     console.log(`MikeApp.js: sending a message from React to Electron`);
     window.ElectronAPI.send(channels.TEST_EVENT_MIKE, { product: 'notebook' });
     window.ElectronAPI.send(channels.TO_MAIN, 'Hi from react');
   }
 
-  // Receive data from the main process and update some element in my component
+  // Handler to receive data from the main process and update some element in my component
   useEffect(() => {
     window.ElectronAPI.receive(channels.FROM_MAIN, (data) => {
         console.log(`Received data from main process: ${data}, called from MikeApp.js`);
+        // Update the react component:
+        setMyVal(data);
     });
   }, []); 
 
@@ -24,6 +30,7 @@ function MikeApp() {
         <CPPResultComponent />
         <p>This next button is from MikeApp.js and is for sending info from React to Electron</p>
         <button onClick={sendTestEventMike}>Test Event Mike</button>
+        <p>Value returned from Electron: {myVal}</p>
       </header>
     </div>
   );
